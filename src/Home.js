5617afd738338350a8757c4ee46511724a305d9c
Daniele Ricci <daniele.icc@gmail.com>
2020-04-06 21:29:03 +0200

    bootstrap test

--- src/Home.js
+++ src/Home.js
@@ -1,9 +1,13 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import { Header } from "./Header";
 import Andamento from "./img/Coronavirus-Grafico-Proiezioni-Picco-Andamento.jpg";
 import Grafico from "./img/Coronavirus-Grafico.jpg";
 import Picco from "./img/Coronavirus-Grafico-Proiezioni-Picco.jpg";
 import Proiezioni from "./img/Coronavirus-Grafico-Proiezioni.jpg";
+
+import Jumbotron from "react-bootstrap/Jumbotron";
+import Toast from "react-bootstrap/Toast";
+import Container from "react-bootstrap/Container";
 
 export class Home extends Component {
 	render() {
@@ -99,7 +103,31 @@ export class Home extends Component {
 						temete), quindi per ora, impiego il mio tempo continuando a lavorare per migliorare il grafico aggregato.
 					</p>
 				</div>
+				<Container className="p-3">
+					<Jumbotron>
+						<h1 className="header">Welcome To React-Bootstrap</h1>
+						<ExampleToast>
+							We now have Toasts
+							<span role="img" aria-label="tada">
+								🎉
+							</span>
+						</ExampleToast>
+					</Jumbotron>
+				</Container>
 			</div>
 		);
 	}
 }
+
+const ExampleToast = ({ children }) => {
+	const [show, toggleShow] = useState(true);
+
+	return (
+		<Toast show={show} onClose={() => toggleShow(! show)}>
+			<Toast.Header>
+				<strong className="mr-auto">React-Bootstrap</strong>
+			</Toast.Header>
+			<Toast.Body>{children}</Toast.Body>
+		</Toast>
+	);
+};
